624afcbb49e0bab714ad0e7d15711ad9f81cae22
jjjjakb <jacob@closedmonday.xyz>
2023-02-06 21:18:32 -0800

    latest update ai bg

--- code.js
+++ code.js
@@ -10,38 +10,12 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 };
 figma.showUI(__html__);
 figma.ui.onmessage = (msg) => __awaiter(void 0, void 0, void 0, function* () {
+    var _a, _b, _c, _d, _e, _f, _g;
     if (msg.type === 'create-frame') {
         let startingX = 0;
         let startingY = 0;
         console.log("did it break2");
-        fetch("https://api.openai.com/v1/images/generations", {
-            method: "POST",
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json',
-                'Authorization': 'Bearer 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'
-            },
-            body: JSON.stringify({
-                prompt: "A cute baby sea otter",
-                n: 2,
-                size: "1024x1024"
-            })
-        });
-        const response = yield fetch("https://api.openai.com/v1/images/generations", {
-            method: "POST",
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json',
-                'Authorization': 'Bearer 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'
-            },
-            body: JSON.stringify({
-                prompt: "A cute baby sea otter",
-                n: 1,
-                size: "1024x1024",
-                response_format: "b64_json"
-            })
-        });
-        function base64ToBytesArr(str) {
+        function base64ToBytesUint8Array(str) {
             const abc = [..."ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"]; // base64 alphabet
             let result = [];
             for (let i = 0; i < str.length / 4; i++) {
@@ -50,70 +24,133 @@ figma.ui.onmessage = (msg) => __awaiter(void 0, void 0, void 0, function* () {
                 let bytes = bin.match(/.{1,8}/g).map(x => +('0b' + x));
                 result.push(...bytes.slice(0, 3 - (str[4 * i + 2] == "=") - (str[4 * i + 3] == "=")));
             }
-            return result;
+            return new Uint8Array(result);
         }
-        const imageData = yield response.json();
-        const base64String = imageData.data[0].b64_json;
-        const imageConverted = base64ToBytesArr(base64String);
-        console.log(base64String);
-        console.log(imageConverted);
-        console.log("it works");
-        // figma.createImage(imageData.data[0].b64_json);
-        // .then(response => response.json())
-        // .then(data => console.log(data))
-        // .catch(error => console.error(error));
-        // console.log("did it break3");
-        // const { Configuration, OpenAIApi } = require("openai");
-        // const configuration = new Configuration({
-        //   apiKey: process.env.OPENAI_API_KEY,
-        // });
-        // const openai = new OpenAIApi(configuration);
-        // const response = await openai.createImage({
-        //   prompt: "A cute baby sea otter",
-        //   n: 2,
-        //   size: "1024x1024",
-        // });
-        // console.log(response);
+        // console.log(base64String);
+        // console.log(imageConverted);
+        // const image = figma.createImage(imageConverted)
+        // console.log("it works");
+        // const node = figma.createRectangle()
+        // // Set the fill on the node
+        // node.fills = [
+        //   {
+        //     type: 'IMAGE',
+        //     imageHash: image.hash,
+        //     scaleMode: 'FILL'
+        //   }
+        // ]
+        let twitter = {
+            width: 1024,
+            height: 512,
+            itemSpacing: 64,
+            horizontalPadding: 80,
+            verticalPadding: 80,
+            headlineFontSize: 80,
+            subtextFontSize: 36,
+        };
+        let instagram = {
+            width: 1080,
+            height: 1080,
+            itemSpacing: 112,
+            horizontalPadding: 128,
+            verticalPadding: 128,
+            headlineFontSize: 96,
+            subtextFontSize: 48,
+        };
+        let linkedin = {
+            width: 1920,
+            height: 680,
+            itemSpacing: 40,
+            horizontalPadding: 136,
+            verticalPadding: 120,
+            headlineFontSize: 96,
+            subtextFontSize: 48,
+        };
+        let facebook = {
+            width: 820,
+            height: 312,
+            itemSpacing: 16,
+            horizontalPadding: 72,
+            verticalPadding: 46,
+            headlineFontSize: 48,
+            subtextFontSize: 24,
+        };
+        let instagramStory = {
+            width: 1080,
+            height: 1920,
+            itemSpacing: 64,
+            horizontalPadding: 128,
+            verticalPadding: 176,
+            headlineFontSize: 128,
+            subtextFontSize: 64,
+        };
         for (const record of msg.values) {
             let assets = [];
             // let parent = new Node({type:'GROUP'})
-            const frameSize = { twitter: [1024, 512], instagram: [1080, 1080], linkedin: [1920, 680], facebook: [820, 312], instagramStory: [1080, 1920] };
-            for (let i = 0; i < Object.keys(frameSize).length; i++) {
-                const testframe = figma.createFrame();
-                console.log(testframe.parent);
-                let frameWidth = frameSize[Object.keys(frameSize)[i]][0];
-                let frameHeight = frameSize[Object.keys(frameSize)[i]][1];
-                testframe.name = `frame${frameWidth}x${frameHeight}`;
-                testframe.x = startingX;
-                testframe.y = startingY;
+            const platformProperties = { twitter, instagram, linkedin, facebook, instagramStory };
+            //this is what generates the image from AI
+            const response = yield fetch("https://api.openai.com/v1/images/generations", {
+                method: "POST",
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+                    'Authorization': 'Bearer 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'
+                },
+                body: JSON.stringify({
+                    prompt: record[1],
+                    n: 1,
+                    size: "1024x1024",
+                    response_format: "b64_json"
+                })
+            });
+            const imageData = yield response.json();
+            const base64String = imageData.data[0].b64_json;
+            const imageConverted = base64ToBytesUint8Array(base64String);
+            for (let i = 0; i < Object.keys(platformProperties).length; i++) {
+                const frame = figma.createFrame();
+                console.log(frame.parent);
+                let platforms = Object.keys(platformProperties);
+                let currentPlatform = platforms[i];
+                let frameWidth = (_a = platformProperties[currentPlatform]) === null || _a === void 0 ? void 0 : _a.width;
+                let frameHeight = (_b = platformProperties[currentPlatform]) === null || _b === void 0 ? void 0 : _b.height;
+                frame.name = `frame${frameWidth}x${frameHeight}`;
+                frame.x = startingX;
+                frame.y = startingY;
                 startingX += frameWidth + 80;
-                testframe.resize(frameWidth, frameHeight);
+                frame.resize(frameWidth, frameHeight);
+                frame.layoutPositioning = 'AUTO';
+                frame.layoutMode = 'VERTICAL';
+                frame.primaryAxisSizingMode = 'FIXED';
+                frame.counterAxisSizingMode = 'FIXED';
+                frame.horizontalPadding = (_c = platformProperties[currentPlatform]) === null || _c === void 0 ? void 0 : _c.horizontalPadding;
+                frame.verticalPadding = (_d = platformProperties[currentPlatform]) === null || _d === void 0 ? void 0 : _d.verticalPadding;
+                frame.itemSpacing = (_e = platformProperties[currentPlatform]) === null || _e === void 0 ? void 0 : _e.itemSpacing;
                 const headlineText = figma.createText();
                 let testFont = { family: 'Inter', style: 'Regular' };
                 yield figma.loadFontAsync(testFont);
-                headlineText.x = testframe.width / 8;
-                headlineText.y = testframe.height / 8;
+                // headlineText.x = frame.width / 8;
+                // headlineText.y = frame.height / 8;
                 headlineText.characters = record[0];
-                headlineText.fontSize = 96;
+                headlineText.fontSize = (_f = platformProperties[currentPlatform]) === null || _f === void 0 ? void 0 : _f.headlineFontSize;
                 headlineText.fills = [{ type: 'SOLID', color: { r: 0, g: 0, b: 0 } }];
-                testframe.appendChild(headlineText);
+                frame.appendChild(headlineText);
                 const subText = figma.createText();
                 yield figma.loadFontAsync(testFont);
-                subText.x = testframe.width / 8;
-                subText.y = testframe.height / 3;
+                // subText.x = frame.width / 8;
+                // subText.y = frame.height / 3;
                 subText.resizeWithoutConstraints(frameWidth / 1.5, frameHeight / 3);
                 subText.characters = record[1];
-                subText.fontSize = 48;
+                subText.fontSize = (_g = platformProperties[currentPlatform]) === null || _g === void 0 ? void 0 : _g.subtextFontSize;
                 subText.fills = [{ type: 'SOLID', color: { r: 0, g: 0, b: 0 } }];
-                testframe.appendChild(subText);
-                const image = yield figma.createImageAsync('https://picsum.photos/200');
+                frame.appendChild(subText);
+                const image = figma.createImage(imageConverted);
                 // Get the size of the image
                 const { width: imageWidth, height: imageHeight } = yield image.getSizeAsync();
                 // Set the fill of the frame to be the image
-                testframe.fills = [
+                frame.fills = [
                     { type: 'IMAGE', imageHash: image.hash, scaleMode: 'FILL' }, { type: 'SOLID', color: { r: 244 / 255.0, g: 136 / 255.0, b: 31 / 255.0 }, opacity: 0.5 }
                 ];
-                assets.push(testframe);
+                assets.push(frame);
             }
             const assetGroup = figma.group(assets, assets[0].parent);
             startingY += assetGroup.height + 50;
--- code.ts
+++ code.ts
@@ -8,145 +8,177 @@ figma.ui.onmessage = async msg => {
     console.log("did it break2");
 
 
-    fetch("https://api.openai.com/v1/images/generations", {
 
-      method: "POST",
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer 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'
-      },
-      body: JSON.stringify({
-        prompt: "A cute baby sea otter",
-  n: 2,
-  size: "1024x1024"
-      })
+
+
+    function base64ToBytesUint8Array(str:string) {
+      const abc = [..."ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"]; // base64 alphabet
+      let result = [];
+
+      for (let i = 0; i < str.length / 4; i++) {
+        let chunk = [...str.slice(4 * i, 4 * i + 4)]
+        let bin = chunk.map(x => abc.indexOf(x).toString(2).padStart(6, 0)).join('');
+        let bytes = bin.match(/.{1,8}/g).map(x => +('0b' + x));
+        result.push(...bytes.slice(0, 3 - (str[4 * i + 2] == "=") - (str[4 * i + 3] == "=")));
+      }
+
+      return new Uint8Array(result);
     }
-  )
-  
-  
-  const response = await fetch("https://api.openai.com/v1/images/generations", {
-
-  method: "POST",
-  headers: {
-    'Accept': 'application/json',
-    'Content-Type': 'application/json',
-    'Authorization': 'Bearer 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'
-  },
-  body: JSON.stringify({
-    prompt: "A cute baby sea otter",
-n: 1,
-size: "1024x1024",
-response_format:"b64_json"
-  })
-}
-)
-
-function base64ToBytesArr(str) {
-  const abc = [..."ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"]; // base64 alphabet
-  let result = [];
-
-  for(let i=0; i<str.length/4; i++) {
-    let chunk = [...str.slice(4*i,4*i+4)]
-    let bin = chunk.map(x=> abc.indexOf(x).toString(2).padStart(6,0)).join(''); 
-    let bytes = bin.match(/.{1,8}/g).map(x=> +('0b'+x));
-    result.push(...bytes.slice(0,3 - (str[4*i+2]=="=") - (str[4*i+3]=="=")));
-  }
-  return result;
-}
-
-const imageData = await response.json();
-const base64String = imageData.data[0].b64_json;
-const imageConverted = base64ToBytesArr(base64String)
-
-console.log(base64String);
-console.log(imageConverted);
-console.log("it works");
 
 
-// figma.createImage(imageData.data[0].b64_json);
+    // console.log(base64String);
+    // console.log(imageConverted);
+    // const image = figma.createImage(imageConverted)
+    // console.log("it works");
 
+    // const node = figma.createRectangle()
 
-    
-
-
-    
-    // .then(response => response.json())
-    // .then(data => console.log(data))
-    // .catch(error => console.error(error));
-    // console.log("did it break3");
+    // // Set the fill on the node
+    // node.fills = [
+    //   {
+    //     type: 'IMAGE',
+    //     imageHash: image.hash,
+    //     scaleMode: 'FILL'
+    //   }
+    // ]
 
 
 
+    let twitter = {
+      width: 1024,
+      height: 512,
+      itemSpacing: 64,
+      horizontalPadding: 80,
+      verticalPadding: 80,
+      headlineFontSize: 80,
+      subtextFontSize: 36,
+    }
 
+    let instagram = {
+      width: 1080,
+      height: 1080,
+      itemSpacing: 112,
+      horizontalPadding: 128,
+      verticalPadding: 128,
+      headlineFontSize: 96,
+      subtextFontSize: 48,
+    }
 
+    let linkedin = {
+      width: 1920,
+      height: 680,
+      itemSpacing: 40,
+      horizontalPadding: 136,
+      verticalPadding: 120,
+      headlineFontSize: 96,
+      subtextFontSize: 48,
+    }
 
-    // const { Configuration, OpenAIApi } = require("openai");
-    // const configuration = new Configuration({
-    //   apiKey: process.env.OPENAI_API_KEY,
-    // });
-    // const openai = new OpenAIApi(configuration);
-    // const response = await openai.createImage({
-    //   prompt: "A cute baby sea otter",
-    //   n: 2,
-    //   size: "1024x1024",
-    // });
-    // console.log(response);
+    let facebook = {
+      width: 820,
+      height: 312,
+      itemSpacing: 16,
+      horizontalPadding: 72,
+      verticalPadding: 46,
+      headlineFontSize: 48,
+      subtextFontSize: 24,
+    }
+
+    let instagramStory = {
+      width: 1080,
+      height: 1920,
+      itemSpacing: 64,
+      horizontalPadding: 128,
+      verticalPadding: 176,
+      headlineFontSize: 128,
+      subtextFontSize: 64,
+    }
 
     for (const record of msg.values) {
       let assets = [];
       // let parent = new Node({type:'GROUP'})
-      const frameSize = { twitter: [1024, 512], instagram: [1080, 1080], linkedin: [1920, 680], facebook: [820, 312], instagramStory: [1080, 1920] };
+      const platformProperties = { twitter, instagram, linkedin, facebook, instagramStory };
 
-      for (let i = 0; i < Object.keys(frameSize).length; i++) {
-        const testframe = figma.createFrame();
+      //this is what generates the image from AI
+      const response = await fetch("https://api.openai.com/v1/images/generations", {
 
-        console.log(testframe.parent)
+        method: "POST",
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer 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'
+        },
+        body: JSON.stringify({
+          prompt: record[1],
+          n: 1,
+          size: "1024x1024",
+          response_format: "b64_json"
+        })
+      }
+      )
+      const imageData = await response.json();
+      const base64String = imageData.data[0].b64_json;
+      const imageConverted = base64ToBytesUint8Array(base64String)
 
-        let frameWidth = frameSize[Object.keys(frameSize)[i]][0]
-        let frameHeight = frameSize[Object.keys(frameSize)[i]][1]
 
-        testframe.name = `frame${frameWidth}x${frameHeight}`
-        testframe.x = startingX
-        testframe.y = startingY
+      for (let i = 0; i < Object.keys(platformProperties).length; i++) {
+        const frame = figma.createFrame();
+
+        console.log(frame.parent)
+
+        let platforms = Object.keys(platformProperties);
+        let currentPlatform = platforms[i];
+        let frameWidth = platformProperties[currentPlatform]?.width
+        let frameHeight = platformProperties[currentPlatform]?.height
+
+        frame.name = `frame${frameWidth}x${frameHeight}`
+        frame.x = startingX
+        frame.y = startingY
         startingX += frameWidth + 80;
-        testframe.resize(frameWidth, frameHeight);
+        frame.resize(frameWidth, frameHeight);
+        frame.layoutPositioning = 'AUTO';
+        frame.layoutMode = 'VERTICAL';
+        frame.primaryAxisSizingMode = 'FIXED';
+        frame.counterAxisSizingMode = 'FIXED';
+        frame.horizontalPadding = platformProperties[currentPlatform]?.horizontalPadding;
+        frame.verticalPadding = platformProperties[currentPlatform]?.verticalPadding;
+        frame.itemSpacing = platformProperties[currentPlatform]?.itemSpacing;
+
 
 
         const headlineText = figma.createText();
         let testFont = { family: 'Inter', style: 'Regular' }
         await figma.loadFontAsync(testFont);
-        headlineText.x = testframe.width / 8;
-        headlineText.y = testframe.height / 8;
+        // headlineText.x = frame.width / 8;
+        // headlineText.y = frame.height / 8;
         headlineText.characters = record[0];
-        headlineText.fontSize = 96;
+        headlineText.fontSize = platformProperties[currentPlatform]?.headlineFontSize;
         headlineText.fills = [{ type: 'SOLID', color: { r: 0, g: 0, b: 0 } }];
-        testframe.appendChild(headlineText);
+        frame.appendChild(headlineText);
 
 
         const subText = figma.createText();
         await figma.loadFontAsync(testFont);
-        subText.x = testframe.width / 8;
-        subText.y = testframe.height / 3;
+        // subText.x = frame.width / 8;
+        // subText.y = frame.height / 3;
         subText.resizeWithoutConstraints(frameWidth / 1.5, frameHeight / 3)
         subText.characters = record[1];
-        subText.fontSize = 48;
+        subText.fontSize = platformProperties[currentPlatform]?.subtextFontSize;
         subText.fills = [{ type: 'SOLID', color: { r: 0, g: 0, b: 0 } }];
-        testframe.appendChild(subText);
+        frame.appendChild(subText);
 
 
-        const image = await figma.createImageAsync(
-          'https://picsum.photos/200'
-        );
+        const image = figma.createImage(imageConverted)
+          ;
 
         // Get the size of the image
         const { width: imageWidth, height: imageHeight } = await image.getSizeAsync();
 
         // Set the fill of the frame to be the image
-        testframe.fills = [
+        frame.fills = [
           { type: 'IMAGE', imageHash: image.hash, scaleMode: 'FILL' }, { type: 'SOLID', color: { r: 244 / 255.0, g: 136 / 255.0, b: 31 / 255.0 }, opacity: 0.5 }];
 
-        assets.push(testframe);
+        assets.push(frame);
       }
       const assetGroup = figma.group(assets, assets[0].parent)
 
